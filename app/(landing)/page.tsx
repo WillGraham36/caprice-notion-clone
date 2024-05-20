33e5336efe056f1b81479a7a4441c7ca9f15d605
Will <willgraham367@gmail.com>
2024-05-20 01:05:51 -0400

    Setup the project

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="text-white bg-emerald-600">
+        Click me!
+      </Button>
+    </div>
+  );
+}
